da272ce13511165295bcbea6c4dc34cb968c2520
kathyyzhang <kathyzhang168@gmail.com>
2015-09-28 22:21:28 -0700

    k

--- XmasTrees/XmasTrees.java
+++ XmasTrees/XmasTrees.java
@@ -13,6 +13,7 @@ public class XmasTrees
     }
     public static void OneRow(int spaces, int stars)
     {
+        //prints out one row of stars and spaces
         for (int i=0; i<spaces; i++)
         {
             System.out.print(" ");
@@ -25,6 +26,7 @@ public class XmasTrees
     }
     public static void TreePart(int startNum, int height, int segment)
     {
+        //prints out one part of a tree
         for (int i=0; i<height; i++)
         {
             OneRow(height-(i+1)+(segment-(startNum-1)/2), startNum+(i*2));
@@ -32,10 +34,12 @@ public class XmasTrees
     }
     public static void PrintTree(int height, int segment)
     {
+        //prints out parts according to number of segments
         for (int i=0; i<segment; i++)
         {
             TreePart(2*i+1, height, segment);
         }
+        //prints base of tree
          OneRow(height+segment-1, 1);
          OneRow(height+segment-1, 1);
          OneRow(height+segment-4, 7);
